d0bb4e5fe22e87e32b799009eacf8c0735c8953c
Al-Jhoenil Wahid <aljhoenilw@gmail.com>
2019-08-25 15:48:48 +0800

    create addStudents method

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,17 @@ class App extends React.Component {
     });
   }
 
-  /*Check out this addAssignment method*/
   addAssignment(assignmentName) {
     this.setState({
       assignments: this.state.assignments.concat(assignmentName)
     });
   }
 
-  /*Write an addStudent method here*/
+  addStudent(studentName) {
+    this.setState({
+      students: this.state.students.concat(studentName)
+    });
+  }
 
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
