df315b0c853199fa4fb1785572e242dc802c566e
Al-Jhoenil Wahid <aljhoenilw@gmail.com>
2019-08-25 19:23:58 +0800

    Updates

---
+++ precache-manifest.a89bb1d421c46fe599e531e8423d6151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59ef112b50befc4421bcef8ff68dd37",
+    "url": "/reactjs-gradebook/index.html"
+  },
+  {
+    "revision": "f3651a94143b135f0523",
+    "url": "/reactjs-gradebook/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/reactjs-gradebook/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "f3651a94143b135f0523",
+    "url": "/reactjs-gradebook/static/js/main.37092d0d.chunk.js"
+  },
+  {
+    "revision": "facf5b1cc67edee1b553",
+    "url": "/reactjs-gradebook/static/js/runtime~main.3f34de52.js"
+  }
+]);
