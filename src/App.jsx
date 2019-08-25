a297f33835cc421bf64de26bbd3499a975067e4f
Al-Jhoenil Wahid <aljhoenilw@gmail.com>
2019-08-25 16:03:32 +0800

    uncomment grades section

--- src/App.jsx
+++ src/App.jsx
@@ -73,8 +73,8 @@ class App extends React.Component {
       );
     }
 
-    /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+   
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -83,7 +83,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
